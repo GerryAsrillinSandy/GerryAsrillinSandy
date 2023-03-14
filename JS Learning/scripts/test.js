8ea08eaa989e3c3a4e6e4d40f2f1804fb31aac21
Gerry Asrillin Sandy <gerryasrillinsandy@yahoo.com>
2023-03-14 17:23:17 +0700

    new update
    new update

---
+++ JS Learning/scripts/test.js
@@ -0,0 +1,27 @@
+const inputAge = prompt("How old are you?")
+const myAge = Number(inputAge) //Convert string to number
+
+function isAgeOlderThan18(age) {
+	if(age > 18) return true
+  else return false 
+  }
+
+function
+checkAllowance(age){
+	const isOldEnough = isAgeOlderThan18(age)
+  	if (isOldEnough) return "You are allowed"
+    else return "You are not allowed"
+      }
+      
+function canHaveNationalID(age){
+	const isOldEnough = isAgeOlderThan18(age)
+  if(isOldEnough) return "You can have national ID"
+  else return "You can not have national ID yet"
+  }
+
+const isAllowed = checkAllowance(myAge)
+const canHaveID = canHaveNationalID(myAge)
+
+console.log("My age is",myAge,"years old")
+console.log("Am I allowed?", isAllowed)
+console.log("Can I have National ID?",canHaveID)
---
+++ Python Learning/data_structure_lecture.py
@@ -0,0 +1,28 @@
+""" first_name = 'josh'
+last_name = 'thomas'
+age = 17
+gender = 'male'
+
+print(first_name)
+ """
+
+"""
+first_name, last_name, age = 'Julia', 'Aluna', 20
+"""
+
+""" first_name = input('What is your first name: ')
+last_name = input('What is your last name: ')
+age = int(input('How old are you: ')) """
+
+""" def my_function(x):
+    return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9)) """
+
+import numpy as np
+
+
+repeat_array = np((4, 3), [1,2,3], dtype=int)
+print (repeat_array)
