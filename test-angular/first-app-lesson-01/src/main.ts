c9c9529e0e7cb22578c84d80701331e98f8fb0da
Gerry Asrillin Sandy <gerryasrillinsandy@yahoo.com>
2023-05-31 14:52:47 +0700

    q

--- test-angular/first-app-lesson-01/src/main.ts
+++ test-angular/first-app-lesson-01/src/main.ts
@@ -1,10 +1,14 @@
 /*
-*  Protractor support is deprecated in Angular.
-*  Protractor is used in this example for compatibility with Angular documentation tools.
-*/
-import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+ *  Protractor support is deprecated in Angular.
+ *  Protractor is used in this example for compatibility with Angular documentation tools.
+ */
+import {
+  bootstrapApplication,
+  provideProtractorTestingSupport,
+} from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
-  .catch(err => console.error(err));
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
