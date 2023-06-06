dc317da2e82ab20036855c09dba13cd7cb431886
Gerry Asrillin Sandy <gerryasrillinsandy@yahoo.com>
2023-06-06 20:04:25 +0700

    2

--- D2-JS-Implementation-1/practice2/script.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementById("submitButton");
-var textInput = document.getElementById("textInput");
-var result = document.getElementById("result");
-
-button.addEventListener("click", function () {
-  var inputValue = textInput.value;
-  result.innerHTML = "You entered: " + inputValue;
-});
