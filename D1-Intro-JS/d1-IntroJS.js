c6ddafcf32037f95b8374141b0c082b2cb589f4e
Gerry Asrillin Sandy <gerryasrillinsandy@yahoo.com>
2023-09-05 21:04:06 +0700

    1

--- D1-Intro-JS/d1-IntroJS.js
+++ D1-Intro-JS/d1-IntroJS.js
@@ -1,64 +1,65 @@
 //variable
-// var string = "Hello";
-// let number = 5;
-// const boolean = true;
+var string = "Hello";
+let number = 5;
+const boolean = true;
 
-// console.log("var string" + string);
-// console.log("var number" + number);
-// console.log("var boolean" + boolean);
+console.log("var string" + string);
+console.log("var number" + number);
+console.log("var boolean" + boolean);
 
 //contoh array & object
-// let array = [0, 1, 2, 3, 4, 5];
-// console.log("var array : " + array);
+let array = [0, 1, 2, 3, 4, 5];
+console.log("var array : " + array);
 
-// let object = {
-//   name: "Alvian",
-//   age: 31,
-//   isMarried: true,
-// };
+let object = {
+  name: "Alvian",
+  age: 31,
+  isMarried: true,
+};
 
-// console.log("Nama : " + object.name);
-// console.log("Age : " + object.age);
-// console.log("Married Status " + object.isMarried);
+console.log("Nama : " + object.name);
+console.log("Age : " + object.age);
+console.log("Married Status " + object.isMarried);
 
 //Contoh Operator
-// let myNumber = "100";
-// let myLet = 100;
+let myNumber = "100";
+let myLet = 100;
 
-// let isEqual = (myLet = myNumber); // equal to
-// console.log(typeof isEqual + " " + isEqual);
+let isEqual = (myLet = myNumber); // equal to
+console.log(typeof isEqual + " " + isEqual);
 
-// let isEqual2 = myNumber == myLet; // equal to
-// console.log(typeof isEqual2 + " " + isEqual2);
+let isEqual2 = myNumber == myLet; // equal to
+console.log(typeof isEqual2 + " " + isEqual2);
 
-// let isEqual3 = myNumber === myLet; // equal to
-// console.log(typeof isEqual3 + " " + isEqual3);
+let isEqual3 = myNumber === myLet; // equal to
+console.log(typeof isEqual3 + " " + isEqual3);
 
-// let isNotEqual = myNumber != myLet; // not equal to
-// console.log(isNotEqual);
+let isNotEqual = myNumber != myLet; // not equal to
+console.log(isNotEqual);
 
-// let isGreaterThan = myNumber > myLet; // greater than
-// console.log(isGreaterThan);
+let isGreaterThan = myNumber > myLet; // greater than
+console.log(isGreaterThan);
 
-// let isLessThan = myNumber < myLet; // less than
-// console.log(isLessThan);
+let isLessThan = myNumber < myLet; // less than
+console.log(isLessThan);
 
-// let isGreaterThanOrEqual = myNumber >= myLet; // greater than or equal to
-// console.log(isGreaterThanOrEqual);
+let isGreaterThanOrEqual = myNumber >= myLet; // greater than or equal to
+console.log(isGreaterThanOrEqual);
 
-// let isLessThanOrEqual = myNumber <= myLet; // less than or equal to
-// console.log(isLessThanOrEqual);
+let isLessThanOrEqual = myNumber <= myLet; // less than or equal to
+console.log(isLessThanOrEqual);
 
-// let namaBelakang = "El Hakim";
-// namaBelakang = "Cahyadi";
+// test variable let
+let namaBelakang = "El Hakim";
+namaBelakang = "Cahyadi";
 
-// // if (true) {
-// //   let namaBelakang = "Cahyadi";
+if (true) {
+  let namaBelakang = "Cahyadi";
 
-// //   console.log(namaBelakang); // Akan menghasilkan 'Cahyadi'
-// // }
+  console.log(namaBelakang); // Akan menghasilkan 'Cahyadi'
+}
 
-// console.log(namaBelakang); // Akan menghasilkan 'El Hakim'
+console.log(namaBelakang); // Akan menghasilkan 'El Hakim'
 
 //Control Flow
 // Conditional Statements
