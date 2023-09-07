729a127b81018fd59d83156a4bdfe1e2158f6fe5
Gerry Asrillin Sandy <gerryasrillinsandy@yahoo.com>
2023-09-07 10:51:54 +0700

    1

---
+++ D1-Intro-JS/arrayAndObject.js
@@ -0,0 +1,31 @@
+// const pets = ["cat", "dog", "canary", "turtle"];
+
+// console.log(pets);
+// console.log(pets[1]);
+
+// for (var i = 0; i < pets.length; i++) {
+//   console.log(pets[i]);
+// }
+
+// for (let index in pets) {
+//   console.log(pets[index]);
+// }
+
+// const person = {
+//   name: "John Doe",
+//   age: 30,
+//   profession: "Developer",
+// };
+
+// const students = [
+//   { name: "John", age: 20, grade: "A" },
+//   { name: "Jane", age: 19, grade: "B" },
+//   { name: "Sam", age: 21, grade: "A+" },
+//   { name: "Emily", age: 18, grade: "B+" },
+// ];
+
+// const myStudent = students.map(function (student) {
+//   return student.name;
+// });
+
+// console.log(myStudent);
