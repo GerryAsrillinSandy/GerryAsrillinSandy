729263ca07fe51dfec14aa9e09a116f212482272
Gerry Asrillin Sandy <gerryasrillinsandy@yahoo.com>
2025-05-12 13:09:41 +0000

    commit

--- fundamental-class/selectedScript.js
+++ fundamental-class/selectedScript.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Prompt untuk mengganti nama
+  // Variable di JS = VAR, LET, CONST
   const name = window.prompt("Masukkan nama Anda:", "Harfi");
   if (name) {
     const span = document.querySelector(".highlight");
     if (span) span.textContent = name;
+    console.log('name : '+ name)
   }
 })
